76541c629d6594e9eda5b6de1e29f2ecfbee0f81
Lavkush Maurya <lavkushmaurya147@gmail.com>
2023-06-29 18:00:51 +0530

    last

---
+++ components/Layout.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+// import Preloader from "./Pre";
+
+const Layout = ({ children }) => {
+  // const [load, updateLoad] = useState(true);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     updateLoad(false);
+  //   }, 1200);
+  //   return () => clearTimeout(timer);
+  // }, []);
+
+  return (
+    <>
+      <nav>Logo</nav>
+      {/* <Preloader load={load} /> */}
+      <div>{children}</div>
+    </>
+  );
+};
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/global.css";
+import Preloader from "../components/Pre";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const [load, updateLoad] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 1200);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <ChakraProvider>
+      <Preloader load={load} />
       <Component {...pageProps} />
     </ChakraProvider>
   );
