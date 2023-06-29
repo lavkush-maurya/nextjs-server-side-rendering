5d22c02b6c3072f8e247a96a01c12e788fec6d5d
Lavkush Maurya <lavkushmaurya147@gmail.com>
2023-06-29 17:14:59 +0530

    changes in componets

--- components/PreviousPage.jsx
+++ components/PreviousPage.jsx
@@ -6,8 +6,8 @@ import { IconButton } from "@chakra-ui/react";
 const PreviousPage = () => {
   const router = useRouter();
 
-  const handleGoBack = () => {
-    const currentUserId = router.query.id; // Get the current user ID from the router query
+  const handleGoBack = async () => {
+    const currentUserId = await router.query.id; // Get the current user ID from the router query
     const nextUserId = parseInt(currentUserId, 10) - 1; // Increment the user ID by 1
 
     // Generate the new route with the updated user ID
